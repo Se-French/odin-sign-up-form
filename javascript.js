25f667619147dd477b31d88bd3ffb8d8b269ecdd
Se-French <116462983+Se-French@users.noreply.github.com>
2023-04-18 20:35:34 -0400

    Edit HTML, style, and javascript for invalid email styles

--- javascript.js
+++ javascript.js
@@ -4,17 +4,29 @@ function validate() {
     const message = document.getElementById('error-message');
     const submit = document.getElementById('submit');
 
-    if (password.value == confirm_pw.value || 
-                confirm_pw.value == '') {
+    if (password.value == confirm_pw.value || confirm_pw.value == '') {
         message.className = '';
+        confirm_pw.className = "";
         submit.disabled = false;
         submit.style.color = 'rgb(255, 255, 255)'
-        confirm_pw.className = "";
     } else if (password.value != confirm_pw.value) {
-        message.className = 'error-message'
+        message.className = 'error-message';
+        confirm_pw.className = "error";
         submit.disabled = true;
         submit.style.color = "grey";
-        confirm_pw.className = "error";
+    }
+}
+
+function validateEmail() {
+    const email = document.getElementById('email');
+    const email_error = document.getElementById('email-error');
+
+    if (email.value.includes('@') == true || email.value == '') {
+        email.className = '';
+        email_error.className = ''
+    } else if (email.value.includes('@') == false) {
+        email.className = 'error';
+        email_error.className = 'email-error'
     }
 }
 
